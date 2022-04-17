ba41c581a1f6f044170530f50abaa97bda75f592
Yaron Veg <yaronveg1@gmail.com>
2022-04-17 13:22:08 +0300

    passing user items to users via props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
+        <Users />
       </div>
     );
   }
