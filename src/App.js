b4e78cfd8468633728f495499f221901492c3e66
Yaron Veg <Yaronveg1@gmail.com>
2022-04-17 18:27:38 +0300

    spinner and env vars

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import axios from "axios";
 
-const getUsersUrl = "https://api.github.com/users";
+const clientId = `?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}`;
+const clientSecret = `&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+const getUsersUrl = `https://api.github.com/users${clientId}${clientSecret}`;
 
 class App extends Component {
   state = {
