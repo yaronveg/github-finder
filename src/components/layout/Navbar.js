6aff479e6a90dd1c4668571e820130a237366ebc
Yaron Veg <yaronveg1@gmail.com>
2022-04-17 15:51:43 +0300

    lesson 15 midway checkpoint

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,25 +1,25 @@
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { Component } from "react";
 import PropTypes from "prop-types";
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-    icon: faGithub,
-  };
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.object.isRequired,
-  };
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <FontAwesomeIcon icon={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+import React from "react";
+
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <FontAwesomeIcon icon={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: faGithub,
+};
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.object.isRequired,
+};
 
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,25 +1,26 @@
-import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm m-y-1">
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm m-y-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.prototype = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
