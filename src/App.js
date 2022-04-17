8078a56a747c0a417d15849f659b66af0f9a3bf3
Yaron Veg <Yaronveg1@gmail.com>
2022-04-17 10:38:32 +0300

    navbar creation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
-function App() {
-  return <div className="App">hello world</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
