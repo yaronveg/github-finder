cdb32fda2ca168d71896a09b6d51d020b7d95caa
Yaron Veg <Yaronveg1@gmail.com>
2022-04-18 18:45:59 +0300

    react router not working yet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import axios from 'axios';
+import React, { Component, Fragment } from 'react';
+import './App.css';
 import { Alert } from './components/layout/Alert';
+import Navbar from './components/layout/Navbar';
+import Search from './components/users/Search';
+import Users from './components/users/Users';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { Switch } from 'react-router';
 
 const baseUrl = `https://api.github.com/`;
 const clientId = `client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}`;
 const clientSecret = `client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
-// const getUsersUrl = `${baseUrl}users?${clientId}&${clientSecret}`;
 const searchUsersBaseUrl = `${baseUrl}search/users`;
 
 class App extends Component {
@@ -45,19 +46,31 @@ class App extends Component {
     const { users, loading, alert } = this.state;
 
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            setAlert={this.setAlert}
-            showClear={users.length > 0}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      setAlert={this.setAlert}
+                      showClear={users.length > 0}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
