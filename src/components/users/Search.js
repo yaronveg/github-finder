81941bc3a087be3416f54b15f0da12a19e657cfa
Yaron Veg <Yaronveg1@gmail.com>
2022-04-18 10:19:29 +0300

    search function propType

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,14 +1,19 @@
-import React, { Component } from "react";
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 class Search extends Component {
   state = {
-    text: "",
+    text: '',
   };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  };
+
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
   onSubmit = (e) => {
     e.preventDefault();
     this.props.searchUsers(this.state.text);
-    this.setState({ text: "" });
+    this.setState({ text: '' });
   };
 
   render() {
